21d1de1a196477b8247108c5c8a6397565de35f5
hikaru0214 <hikaru.yoshida.0214@gmail.com>
2024-01-19 22:56:54 +0900

    a

--- src/db-connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const SERVER = 'mysql214.phy.lolipop.lan';
-    const DBNAME = 'LAA1517427-shop';
-    const USER = 'LAA1517427';
-    const PASS = 'Pass0214';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
